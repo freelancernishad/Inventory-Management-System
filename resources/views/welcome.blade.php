7c41b3843191e4735f1509a04ea878a3e2330672
Softlabltd.com <freelancernishad123@gmail.com>
2022-06-22 12:19:21 +0600

    logo update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,9 +56,9 @@ span.money {
         <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
           <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
             <div class="sidebar-brand-icon">
-              <img src="{{ asset('backend/img/logo/logo2.png') }}">
+              <img src="{{ asset('m.png') }}">
             </div>
-            <div class="sidebar-brand-text mx-3">POS</div>
+            <div class="sidebar-brand-text mx-3">Msronytraders</div>
           </a>
           <hr class="sidebar-divider my-0">
           <li class="nav-item active">
